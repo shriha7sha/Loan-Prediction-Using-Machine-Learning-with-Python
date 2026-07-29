c3be0c8d1dfa9c95ee82704f6d1e41319d9b4a17
Shriharsha <shriharsha0901@gmail.com>
2026-07-30 04:08:21 +0530

    Add URL routes

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+path("", views.home, name="home"),
+
+path("login/", views.login, name="login"),
+
+path("register/", views.register, name="register"),
+
+path("prediction/", views.prediction, name="prediction"),
+
+path("result/", views.result, name="result"),
+
+]
