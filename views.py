820bab7c3db8111ff93fce350f197a55a106612b
Shriharsha <shriharsha0901@gmail.com>
2026-07-30 04:07:28 +0530

    Create project views

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def prediction(request):
+    return render(request, "prediction.html")
+
+def result(request):
+    return render(request, "result.html")
